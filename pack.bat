e956ca3bd8660b0422afc1bfaca96d8679db39a8
woodenarow <louzhihao@huawei.com>
2026-05-11 09:41:52 +0800

    add pack.bat

---
+++ pack.bat
@@ -0,0 +1,9 @@
+::windows下只运行这个脚本就行了
+set SCRIPT_DIR=%~dp0
+
+rmdir /s /q "%SCRIPT_DIR%package\src\main\ets" 2>nul
+rmdir /s /q "%SCRIPT_DIR%dist" 2>nul
+xcopy "%SCRIPT_DIR%native_ability\src\main\ets\*" "%SCRIPT_DIR%package\src\main\ets\" /E /I /Y >nul
+
+tar -czf ability.har package
+pause
